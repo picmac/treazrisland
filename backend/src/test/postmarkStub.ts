744f313cab5948c9869fd5ce965335e0a3e7e511
picmac <41627742+picmac@users.noreply.github.com>
2025-10-30 13:54:47 +0100

    Add observability rate limit metric and regression test

---
+++ backend/src/test/postmarkStub.ts
@@ -0,0 +1,7 @@
+export class ServerClient {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  constructor(_token: string) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async sendEmail(_payload: unknown): Promise<void> {}
+}
